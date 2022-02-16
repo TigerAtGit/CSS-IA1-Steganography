4c5aac03ef2e650f05b0eae74f586d5c71dd40eb
vishal <68952732+vishalr-22@users.noreply.github.com>
2022-02-16 22:44:12 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,21 @@ app = Flask(__name__)
 @app.route('/',methods=['POST','GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/', methods = ['POST', 'GET'])
+def encode():
+    if request.method == 'POST':
+        text = request.form.get('text1')
+        uploadfile = request.form.get('uploadfile')
+        uploadimage = request.form.get('uploadimage')
+        
+    return render_template('signinpage.html')
+
+
+@app.route('/', methods = ['POST', 'GET'])
+def decode():
+    if request.method == 'POST':
+        inputimage = request.form.get('inputimage')
+
+        
+    return render_template('signinpage.html')
