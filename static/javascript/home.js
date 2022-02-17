51fb997172d121ae88e7fee750e182f6c6241172
vishal <68952732+vishalr-22@users.noreply.github.com>
2022-02-17 17:11:50 +0530

    ui changed

--- static/javascript/home.js
+++ static/javascript/home.js
@@ -1,5 +1,33 @@
+const actualBtn = document.getElementById('actualbtn');
+
+const fileChosen = document.getElementById('filechosen');
+
+actualBtn.addEventListener('change', function() {
+    fileChosen.textContent = this.files[0].name
+})
+
 document.querySelector('.img__btn').addEventListener('click', function() {
     document.querySelector('.cont').classList.toggle('s--signup');
 });
 
 $('#OpenImgUpload').click(function() { $('#imgupload').trigger('click'); });
+
+
+var inputs = document.querySelectorAll('.inputfile');
+Array.prototype.forEach.call(inputs, function(input) {
+    var label = input.nextElementSibling,
+        labelVal = label.innerHTML;
+
+    input.addEventListener('change', function(e) {
+        var fileName = '';
+        if (this.files && this.files.length > 1)
+            fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+        // else{
+        // 	fileName = e.target.value.split( '\' ).pop();}
+
+        if (fileName)
+            label.querySelector('span').innerHTML = fileName;
+        else
+            label.innerHTML = labelVal;
+    });
+});
